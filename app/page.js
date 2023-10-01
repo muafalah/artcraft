fa3e3dce9e997ff4bc30c3aecac0931cd01d409b
muafalah <muh.ifa.amrillah@gmail.com>
2023-10-02 02:57:43 +0700

    feat: add how it works component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
 
 export default function Home() {
   return (
     <>
       <Hero className="pt-32 pb-0 lg:pb-10" />
-      <About className="ppy-16 lg:py-32 overflow-hidden" />
+      <About className="py-16 lg:py-32 overflow-hidden" />
+      <HowItWorks className="py-16 !pt-32 lg:py-32" />
     </>
   );
 }
